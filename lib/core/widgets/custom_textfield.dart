64eec24a183efc82c1155541f064c54777c7cfa7
ankitask <ankitakalangutkar90@gmail.com>
2024-08-08 13:12:06 +0200

    Upload song UI

--- lib/core/widgets/custom_textfield.dart
+++ lib/core/widgets/custom_textfield.dart
@@ -2,18 +2,25 @@ import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget {
   final String hintText;
-  final TextEditingController controller;
+  final TextEditingController? controller;
   final bool isObsecure;
+  final bool readOnly;
+  final VoidCallback? onTap;
 
-  const CustomTextfield(
-      {super.key,
-      required this.hintText,
-      required this.controller,
-      this.isObsecure = false});
+  const CustomTextfield({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObsecure = false,
+    this.readOnly = false,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onTap: onTap,
+      readOnly: readOnly,
       decoration: InputDecoration(
         hintText: hintText,
       ),
--- lib/features/home/view/pages/upload_song_page.dart
+++ lib/features/home/view/pages/upload_song_page.dart
@@ -1,14 +1,116 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/widgets/custom_textfield.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UploadSongPage extends ConsumerWidget {
+class UploadSongPage extends ConsumerStatefulWidget {
   const UploadSongPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<UploadSongPage> createState() => _UploadSongPageState();
+}
+
+class _UploadSongPageState extends ConsumerState<UploadSongPage> {
+  final songNameController = TextEditingController();
+  final artistController = TextEditingController();
+  Color selectedColor = Pallete.cardColor;
+
+  @override
+  void dispose() {
+    songNameController.dispose();
+    artistController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Song'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.check,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              DottedBorder(
+                color: Pallete.borderColor,
+                radius: const Radius.circular(10),
+                borderType: BorderType.RRect,
+                dashPattern: const [10, 4],
+                strokeCap: StrokeCap.round,
+                child: const SizedBox(
+                  height: 150,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.folder_open,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Select the Thumbnail your song',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              CustomTextfield(
+                hintText: 'Pick a song',
+                controller: null,
+                readOnly: true,
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomTextfield(
+                hintText: 'Artist',
+                controller: artistController,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomTextfield(
+                hintText: 'Song Name',
+                controller: songNameController,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ColorPicker(
+                pickersEnabled: const {
+                  ColorPickerType.wheel: true,
+                },
+                color: selectedColor,
+                onColorChanged: (Color color) {
+                  setState(() {
+                    selectedColor = color;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
