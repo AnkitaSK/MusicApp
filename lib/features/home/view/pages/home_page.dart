95c7ac5da97fe833a5386fcc88471aa38a422605
ankitask <ankitakalangutkar90@gmail.com>
2024-08-11 19:44:02 +0200

    added BottomNavigationBar

--- lib/features/home/view/pages/home_page.dart
+++ lib/features/home/view/pages/home_page.dart
@@ -1,11 +1,45 @@
+import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
+  // int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        // currentIndex: selectedIndex,
+        onTap: (value) {
+          // setState(() {
+          //   selectedIndex = value;
+          // });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/home_filled.png'),
+            // selectedIndex == 0
+            // ? 'assets/images/home_filled.png'
+            // : 'assets/images/home_unfilled.png',
+            // color: selectedIndex == 0
+            //     ? Pallete.whiteColor
+            //     : Pallete.inactiveBottomBarItemColor,
+            // ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/library.png'),
+            // 'assets/images/library.png',
+            // color: selectedIndex == 1
+            //     ? Pallete.whiteColor
+            //     : Pallete.inactiveBottomBarItemColor,
+            // ),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
   }
 }
